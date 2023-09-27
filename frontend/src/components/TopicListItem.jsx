63f7cfa635345db20176afcd2d220e07822d38f3
RonnieGuo <guorongfly@gmail.com>
2023-09-26 23:11:14 -0600

    TopicListItem

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -2,12 +2,17 @@ import React from "react";
 
 import "../styles/TopicListItem.scss";
 
-const TopicListItem = ({ topic, updatePhotosByTopics }) => {
+const TopicListItem = ({ id, label, link, onClick }) => {
+  const handleTopicClick = (event) => {
+    event.preventDefault();
+    onClick(id);
+  };
 
-const TopicListItem = () => {
   return (
-    <div className="topic-list__item" onClick={() => updatePhotosByTopics(true, topic)}>
-      <span>{topic.title}</span>
+    <div className="topic-list--item">
+      <a key={id} id={id} label={label} href={link} onClick={handleTopicClick}>
+        <span>{label}</span>
+      </a>
     </div>
   );
 };

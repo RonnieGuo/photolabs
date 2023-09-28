49c6d7d37b52c13067ce016da4ceb370059a393e
RonnieGuo <guorongfly@gmail.com>
2023-09-27 22:17:41 -0600

    Edited

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,57 +1,30 @@
-import React from "react";
-import TopNavigation from "../components/TopNavigationBar";
-import PhotoList from "../components/PhotoList";
-import "../styles/HomeRoute.scss";
+import React from 'react';
+import '../styles/HomeRoute.scss';
+import PhotoList from '../components/PhotoList';
+import TopNavigationBar from '../components/TopNavigationBar';
 
 const HomeRoute = ({
-  photos,
   topics,
+  favorites,
+  toggleFavorite,
+  getPhotosByTopic,
+  photos,
   openModal,
-  isFavorite,
-  addToFavorites,
-  removeFromFavorites,
-  favoritePhotos,
-  handleTopicClick,
-}) => {
-
-  const favoriteCount = favoritePhotos ? favoritePhotos.length : 0;
-
-  const toggleFavorite = (photoId) => {
-    if (isFavorite(photoId)) {
-      removeFromFavorites(photoId);
-    } else {
-      addToFavorites(photoId);
-    }
-  };
-
-  return (
-    <div className="home-route">
-      <div className="nav-bar">
-        <TopNavigation
-          topics={topics}
-          favoriteCount={favoriteCount}
-          isFavPhotoExist={favoriteCount > 0}
-          favoritePhotos={favoritePhotos}
-          photos={photos}
-          handleTopicClick={handleTopicClick}
-        />
-      </div>
-
-      <div className="photo-list">
-        {photos ? (
-          <PhotoList
-            openModal={openModal}
-            photos={photos}
-            isFavorite={isFavorite}
-            addToFavorites={addToFavorites} 
-            removeFromFavorites={removeFromFavorites}
-          />
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
-    </div>
-  );
-};
+}) => (
+  <div className="home-route">
+    <TopNavigationBar
+      topics={topics}
+      favorites={favorites}
+      toggleFavorite={toggleFavorite}
+      getPhotosByTopic={getPhotosByTopic}
+    />
+    <PhotoList
+      photos={photos}
+      openModal={openModal}
+      favorites={favorites}
+      toggleFavorite={toggleFavorite}
+    />
+  </div>
+);
 
 export default HomeRoute;

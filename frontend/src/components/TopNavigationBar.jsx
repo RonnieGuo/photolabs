5b26b555de7799cebc4d8721ffffb40659cae0cc
RonnieGuo <guorongfly@gmail.com>
2023-09-27 19:56:14 -0600

    Edited TopNavigation function

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-
-import '../styles/TopNavigationBar.scss'
+import '../styles/TopNavigationBar.scss';
+import FavBadge from './FavBadge';
+import TopicList from './TopicList'; 
 
 const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <div className="top-nav-bar__icons">
+        <div className="top-nav-bar__icon">
+          <i className="fas fa-heart"></i>
+          <FavBadge count={0} /> 
+        </div>
+        <div className="top-nav-bar__icon">
+          <i className="fas fa-th"></i>
+          <TopicList /> 
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default TopNavigation;

1f044cc9e3976d21ccb3fc1991e8059a2086eaea
RonnieGuo <guorongfly@gmail.com>
2023-10-29 16:24:28 -0600

    Delete unused variables

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,20 +1,26 @@
 import React from "react";
-
 import PhotoListItem from "./PhotoListItem";
 import "../styles/PhotoList.scss";
 
 const PhotoList = (props) => {
-  const { photos, favouritePhotos, addFavourite, delFavourite, openModal, scrollToTop } = props;
+  const {
+    photos,
+    favouritePhotos,
+    addFavourite,
+    delFavourite,
+    openModal,
+    scrollToTop,
+  } = props;
 
   const photoItems = photos.map((photo) => (
     <PhotoListItem
+      key={photo.id}
+      id={photo.id}
       username={photo.user.username}
       city={photo.location.city}
       country={photo.location.country}
       imageSource={photo.urls.regular}
       userAvatar={photo.user.profile}
-      key={photo.id}
-      id={photo.id}
       favouritePhotos={favouritePhotos}
       isFavourite={favouritePhotos.includes(photo.id)}
       addFavourite={addFavourite}

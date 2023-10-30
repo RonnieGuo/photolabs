fe31f5ee97ca9f33ae3b21327bf0fa7d56f86943
RonnieGuo <guorongfly@gmail.com>
2023-10-30 17:24:33 -0600

    Clean the code

--- frontend/src/components/Photo.jsx
+++ frontend/src/components/Photo.jsx
@@ -1,31 +1,41 @@
 import React from "react";
-
 import "../styles/Photo.scss";
 import PhotoFavButton from "./PhotoFavButton";
 
-// component for the image displayed in photodetails modal
 const Photo = (props) => {
+  const {
+    count,
+    favouriteList,
+    toggleFavourite,
+    id,
+    img,
+    profile,
+    username,
+    city,
+    country,
+  } = props;
+
   return (
-    <ol className="photo-ite">
+    <li className="photo-item">
       <div className="image">
         <PhotoFavButton
-          count={props.count}
-          favouriteList={props.favouriteList}
-          toggleFavourite={props.toggleFavourite}
-          id={props.id}
+          count={count}
+          favouriteList={favouriteList}
+          toggleFavourite={toggleFavourite}
+          id={id}
         />
-        <img src={props.img} alt="" className="" />
+        <img src={img} alt={username} />
       </div>
-      <div className="modal_photo-list__user-details">
-        <img src={props.profile} alt="" className="photo-list__user-profile" />
-        <div className="photo-list__user-info">
-          <div>{props.username}</div>
-          <div className="photo-list__user-location">
-            {props.city}, {props.country}
+      <div className="user-details">
+        <img src={profile} alt={username} className="user-profile" />
+        <div className="user-info">
+          <div>{username}</div>
+          <div className="user-location">
+            {city}, {country}
           </div>
         </div>
       </div>
-    </ol>
+    </li>
   );
 };
 
